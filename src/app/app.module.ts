c414b653ea2ba8e71638d3c1b4cfa51f152e7622
Pengtianyu <2501846063@qq.com>
2021-08-24 15:56:41 +0800

    从服务器获取数据

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -20,7 +23,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
 import {Hero} from "./hero";
-import {HEROES} from "./mock-heroes";
 import {Observable, of} from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
 import { MessageService } from "./message.service";
 
 @Injectable({
@@ -9,19 +11,44 @@ import { MessageService } from "./message.service";
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes'
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)
+  }
+
+  /**
+   * @param operation
+   * @param result
+   * */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error)
+      this.log(`${operation} failed: ${error.message}`)
+      return of(result as T)
+    }
+  }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES)
-    this.messageService.add('HeroService: fetched heroes')
-    return heroes
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      )
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)
-    this.messageService.add(`HeroService: fetched hero id=${id}`)
-    // @ts-ignore
-    return of(hero)
+    const url = `${this.heroesUrl}/${id}`
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      )
   }
 
 }
